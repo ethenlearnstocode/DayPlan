1cafef981355a884241980fcbfd17be567aef35c
ethenlearnstocode <ethenlearnstocode@gmail.com>
2026-08-24 08:52:07 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,26 @@
 self.addEventListener('install', (event) => {
-  self.skipWaiting();
+self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim());
+event.waitUntil(self.clients.claim());
 });
 
-// Receive notification trigger from app
 self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SCHEDULE_NOTIFICATION') {
-    const { title, body, delay } = event.data;
-    setTimeout(() => {
-      self.registration.showNotification(title, {
-        body: body,
-        icon: 'https://cdn-icons-png.flaticon.com/512/3652/3652191.png',
-        vibrate: [200, 100, 200]
-      });
-    }, delay);
-  }
+if (event.data && event.data.type === 'SCHEDULE_NOTIFICATION') {
+const { title, body, delay } = event.data;
+
+// Fallback timer inside Service Worker
+setTimeout(() => {
+self.registration.showNotification(title, {
+body: body,
+icon: 'icon.png',
+badge: 'icon.png',
+vibrate: [200, 100, 200],
+tag: 'dayplan-alert',
+renotify: true,
+requireInteraction: true
+});
+}, delay || 1000);
+}
 });
